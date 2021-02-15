5ddfd952b0a66dd102ac4b8a0695cd656d3449bb
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-02-14 22:08:19 -0300

    Save repository

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+
+app.get('/video', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './GraphQL.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize -1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Conten-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+    res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res);
+});
+
+app.listen('3000');
